61073a0adfb939cd5dc6898450825c04145efdae
RAMU <ramu.axeria@gmail.com>
2018-10-06 21:48:58 +0900

    Create New Files

---
+++ maimai_RateCalc/music.js
@@ -0,0 +1,859 @@
+let mas12p = [
+    {
+        "title": "ギリギリ最強あいまいみー！",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "拝啓ドッペルゲンガー",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "人生リセットボタン",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "幸せになれる隠しコマンドがあるらしい",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "初音ミクの消失",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "脳漿炸裂ガール",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "頓珍漢の宴",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "ドーナツホール",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "もうみんなしねばいいのに",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Scream out! -maimai SONIC WASHER Edit-",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "幻想のサテライト",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "儚きもの人間",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "最終鬼畜妹・一部声",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "ウサテイ",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Reach For The Stars",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Like the Wind [Reborn]",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Outlaw's Lullaby",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "ENERGY SYNERGY MATRIX",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "ULTRA B+K",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Brain Power",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "MilK",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "GO BACK 2 YOUR RAVE",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "B.B.K.K.B.K.K.",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "人里に下ったアタイがいつの間にか社畜になっていた件",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Maxi",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Sakura Fubuki",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "METATRON",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "麒麟",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Jack-the-Ripper◆",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "DRAGONLADY",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "電車で電車でOPA!OPA!OPA! -GMT mashup-",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Got more raves？",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "夜明けまであと３秒",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "きたさいたま2000",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Scars of FAUNA",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "FLOWER",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "keep hopping",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Mare Maris",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "デスパレイト",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Ultranova",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "夢花火",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "四月の雨",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "HERA",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Hyper Active",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Jumble Rumble",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Nitrous Fury",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "GEMINI -M-",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "天火明命",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "7thSense",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Lividi",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "閃鋼のブリューナク",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "MYTHOS",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Aiolos",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "DON’T STOP ROCKIN’",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "KING is BACK!!",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Nerverakes",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "火炎地獄",
+        "diff": "Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "The wheel to the right",
+        "diff": "Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "分からない",
+        "diff": "Master",
+        "level": "12.8",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+]
+
+let mas13 = [
+    {
+        "title": "初音ミクの激唱",
+        "diff": "Master",
+        "level": "13.3",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Calamity Fortune",
+        "diff": "Master",
+        "level": "13.2",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Credits",
+        "diff": "Master",
+        "level": "13.3",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "conflict",
+        "diff": "Master",
+        "level": "13.0",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "FUJIN Rumble",
+        "diff": "Master",
+        "level": "13.2",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Ignis Danse",
+        "diff": "Master",
+        "level": "13.0",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Ragnarok",
+        "diff": "Master",
+        "level": "13.0",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "larva",
+        "diff": "Master",
+        "level": "13.4",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Excalibur ～Revived resolution～",
+        "diff": "Master",
+        "level": "13.5",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Caliburne ～Story of the Legendary sword～",
+        "diff": "Master",
+        "level": "13.0",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Justified",
+        "diff": "Master",
+        "level": "13.0",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Moon of Noon",
+        "diff": "Master",
+        "level": "13.3",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Panopticon",
+        "diff": "Master",
+        "level": "13.2",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "ねぇ、壊れタ人形ハ何処へ棄テらレるノ？",
+        "diff": "Master",
+        "level": "13.1",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "AMAZING MIGHTYYYY!!!!",
+        "diff": "Master",
+        "level": "13.3",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "CITRUS MONSTER",
+        "diff": "Master",
+        "level": "13.2",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Axeria",
+        "diff": "Master",
+        "level": "13.3",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "ガラテアの螺旋",
+        "diff": "Master",
+        "level": "13.3",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Our Wrenally",
+        "diff": "Master",
+        "level": "13.4",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Contrapasso -paradiso-",
+        "diff": "Master",
+        "level": "13.2",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Oshama Scramble!",
+        "diff": "Master",
+        "level": "13.0",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Garakuta Doll Play",
+        "diff": "Master",
+        "level": "13.0",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Glorious Crown",
+        "diff": "Master",
+        "level": "13.1",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Fragrance",
+        "diff": "Master",
+        "level": "13.1",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Prophesy One",
+        "diff": "Master",
+        "level": "13.3",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "System “Z”",
+        "diff": "Master",
+        "level": "13.0",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "VERTeX",
+        "diff": "Master",
+        "level": "13.2",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "ジングルベル",
+        "diff": "Master",
+        "level": "13.1",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "エンドマークに希望と涙を添えて",
+        "diff": "Master",
+        "level": "13.1",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "World Vanquisher",
+        "diff": "Master",
+        "level": "13.0",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Xevel",
+        "diff": "Master",
+        "level": "13.1",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+]
+
+let remas12p = [
+    {
+        "title": "POP TEAM EPIC",
+        "diff": "Re:Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "インビジブル",
+        "diff": "Re:Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "ロストワンの号哭",
+        "diff": "Re:Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title" : "サドマミホリック",
+        "diff" : "Re:Master",
+        "level" : "12.7",
+        "score" : "",
+        "achievement" : "",
+        "rate" : ""
+    },
+    {
+        "title": "CYBER Sparks",
+        "diff": "Re:Master",
+        "level": "12.7",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "患部で止まってすぐ溶ける～狂気の優曇華院",
+        "diff": "Re:Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Crush On You",
+        "diff": "Re:Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Sun Dance",
+        "diff": "Re:Master",
+        "level": "12.9",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+]
+
+let remas13 = [
+    {
+        "title": "カゲロウデイズ",
+        "diff": "Re:Master",
+        "level": "13.1",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "ナイト・オブ・ナイツ",
+        "diff": "Re:Master",
+        "level": "13.2",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "In Chaos",
+        "diff": "Re:Master",
+        "level": "13.4",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Beat Of Mind",
+        "diff": "Re:Master",
+        "level": "13.3",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Panopticon",
+        "diff": "Re:Master",
+        "level": "13.2",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "AMAZING MIGHTYYYY!!!!",
+        "diff": "Re:Master",
+        "level": "13.3",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Garakuta Doll Play",
+        "diff": "Re:Master",
+        "level": "13.4",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+    {
+        "title": "Fragrance",
+        "diff": "Re:Master",
+        "level": "13.3",
+        "score": "",
+        "achievement": "",
+        "rate": ""
+    },
+]
